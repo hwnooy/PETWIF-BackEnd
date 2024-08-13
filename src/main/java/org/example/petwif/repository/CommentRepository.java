ce2e7e4aea04875bce5fb8d462995e9db7179957
ddhi7 <144508379+ddhi7@users.noreply.github.com>
2024-08-13 21:26:05 +0900

    Revert "댓글기능 API 구현"

--- src/main/java/org/example/petwif/repository/CommentRepository.java
+++ src/main/java/org/example/petwif/repository/CommentRepository.java
@@ -3,14 +3,10 @@ package org.example.petwif.repository;
 import org.example.petwif.domain.entity.Album;
 import org.example.petwif.domain.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByAlbum(Album album);
-
-
 }
