1234edc0e0af5193415a9f060eebce7fd779003e
choyoungseo20 <whdudtj0809@naver.com>
2024-08-23 18:52:30 +0900

    친구 관계 상태 추가

--- src/main/java/org/example/petwif/domain/enums/FriendStatus.java
+++ src/main/java/org/example/petwif/domain/enums/FriendStatus.java
@@ -4,5 +4,6 @@ public enum FriendStatus {
     PENDING,    // 친구 요청이 발송되었지만 아직 처리되지 않음
     ACCEPTED,   // 친구 요청이 수락됨
     REJECTED,   // 친구 요청이 거부됨
-    CANCELLED  // 친구 요청이 취소됨
+    CANCELLED,  // 친구 요청이 취소됨
+    RECEIVED
 }
