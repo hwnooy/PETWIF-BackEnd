3eb6b2a705584894f4c43c34113bc4cf536aa31c
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-09-12 04:45:58 +0900

    불필요 주석 처리, clientSecret kakaoApi에서 활성화 및 다시 성공

--- src/main/java/org/example/petwif/web/controller/LoginController.java
+++ src/main/java/org/example/petwif/web/controller/LoginController.java
@@ -52,48 +52,4 @@ public class LoginController {
 
         return ApiResponse.onSuccess(dto);
     }
-//    @PostMapping("/oauth")
-//    @ResponseBody
-//    public ApiResponse<TokenDto> kakaoOauth(@RequestParam("code") String code) {
-//        System.out.println("api test");
-//        KakaoTokenResponse kakaoTokenResponse = kakaoTokenJsonData.getToken(code);
-//        KakaoUserInfoResponse userInfo = kakaoUserInfo.getUserInfo(kakaoTokenResponse.getAccess_token());
-//        log.info("회원 정보 입니다.{}", userInfo);
-//        KakaoAccount account = userInfo.getKakao_account();
-//        String email = account.getEmail();
-//
-//        Authentication authentication = new UsernamePasswordAuthenticationToken(email, null);
-//        TokenDto dto = tokenProvider.generateTokenDto(authentication);
-//
-//        userService.createUser(userInfo.getKakao_account().getEmail());
-//
-//        return ApiResponse.onSuccess(dto);
-//    }
-
-
-//    @GetMapping("/oauth")
-//    @ResponseBody
-//    public String kakaoOauth(@RequestParam("code") String code) {
-//        log.info("인가 코드를 이용하여 토큰을 받습니다.");
-//        KakaoTokenResponse kakaoTokenResponse = kakaoTokenJsonData.getToken(code);
-//        log.info("토큰에 대한 정보입니다.{}",kakaoTokenResponse);
-//        KakaoUserInfoResponse userInfo = kakaoUserInfo.getUserInfo(kakaoTokenResponse.getAccess_token());
-//        log.info("회원 정보 입니다.{}",userInfo);
-//
-//        userService.createUser(userInfo.getKakao_account().getEmail());
-//
-//        return "okay";
-//    }
 }
-//    @PostMapping("/code/kakao")
-//    public ApiResponse<TokenDto> kakaoLogin(@RequestParam("code") String code) {
-//        try {
-//            TokenDto dto = kakaoLoginService.signup(code);
-//            return ApiResponse.onSuccess(dto);
-//        } catch (Exception e) {
-//            return null;
-//        }
-//
-//
-//
-//    }
